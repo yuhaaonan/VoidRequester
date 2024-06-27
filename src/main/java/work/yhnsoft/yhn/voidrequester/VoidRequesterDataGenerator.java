4ad356acee836d703a7435cc192706a1670dad82
YHN <m18643950243@163.com>
2024-06-28 00:28:25 +0800

    First Commit

---
+++ src/main/java/work/yhnsoft/yhn/voidrequester/VoidRequesterDataGenerator.java
@@ -0,0 +1,11 @@
+package work.yhnsoft.yhn.voidrequester;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VoidRequesterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
